8e20adce42340b985f9885a22391a0c74558d56d
dburnet3 <deniseburnett26@gmail.com>
2019-08-08 17:23:41 -0400

    setup html route and middleware for authentication

--- client/controllers/htmlRoutes.js
+++ client/controllers/htmlRoutes.js
@@ -1,7 +1,34 @@
 const router = require("express").Router();
 const path = require("path");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
+//---Route for main path/homepage---//
 router.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, "../public/index.html")));
+
+//---Route to redirect the user after login---//
+router.get("/login", function (req, res) {
+    if (req.user) {
+        res.redirect("/activities");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+});
+
+//---Route to redirect the user after signup---//
+router.get("/signup", function (req, res) {
+    if (req.user) {
+        res.redirect("/account");
+    }
+    res.sendFile(path.join(__dirname, "../public/account.html"));
+});
+
+//---Route to direct the user to the account management page---//
+router.get("/account", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/account.html")));
+
+//---Route to direct the user to the activities page once authenticated---//
+router.get("/activities", isAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/activities.html"));
+});
 
 module.exports = router;
