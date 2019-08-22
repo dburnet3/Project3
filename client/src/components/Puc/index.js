824e29dc133cc3335d4df370a72c0d1d662df260
dburnet3 <deniseburnett26@gmail.com>
2019-08-22 15:53:09 -0400

    added skeleton for Puc page

--- client/src/components/Puc/index.js
+++ client/src/components/Puc/index.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import {
+    Container, Col, Form,
+    FormGroup, Label, Input,
+    Button,
+} from 'reactstrap';
 
 class Puc extends Component {
     render() {
         return (
-            <div>
+            <Container className="App">
                 <h2>Manage Person Under Care</h2>
-            </div>
+                <Form className="form">
+                    <Col>
+                        <FormGroup>
+                            <Label>Name</Label>
+                            <Input
+                                type="Puc_name"
+                                name="Puc_name"
+                                id="Puc_name"
+                            />
+                            <Button id="add_Puc">Add</Button>
+                        </FormGroup>
+                    </Col>
+                </Form>
+                <br />
+                <h2>Create Activities for: insert Puc name</h2>
+                <Form className="form">
+                    <Col>
+                        <FormGroup>
+                            <Label>Activity Name</Label>
+                            <Input
+                                type="activity"
+                                name="activity"
+                                id="activity"
+                                placeholder="Feeding"
+                            />
+                            <Label>Time Slot</Label>
+                            <Input
+                                type="time_slot"
+                                name="time_slot"
+                                id="time_slot"
+                                placeholder="Morning"
+                            />
+                            <Button id="add_Puc">Add</Button>
+                        </FormGroup>
+                    </Col>
+                </Form>
+
+            </Container>
         );
     }
 }
