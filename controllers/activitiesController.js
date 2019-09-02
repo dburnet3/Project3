4e83d92ff74ea4bd60ddda6a519bfad7aa322ba5
dburnet3 <deniseburnett26@gmail.com>
2019-09-02 15:29:14 -0400

    made changes to put route for activities

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -11,15 +11,16 @@ module.exports = {
     },
     update: function (req, res) {
         db.Activity
-            // .update({ _id: req.params.id }, { where: req.body })
-            .update(req.body, {
-                where: {
-                    title: req.body.title,
-                    time_slots: req.body.time_slots,
-                    dow: req.body.dow,
-                    taken: req.body.taken
-                }
-            })
+        // .update({ _id: req.params.id }, { where: req.body })
+        // .update(req.body, {
+        //     where: {
+        //         title: req.body.title,
+        //         time_slots: req.body.time_slots,
+        //         dow: req.body.dow,
+        //         taken: req.body.taken
+        //     }
+        // })
+        findOneAndUpdate({ _id: req.params.id }, req.body)
             .then(dbActivity => res.json(dbActivity))
             .catch(err => res.status(422).json(err));
     },
