78277cb3749756d2be1a1170f668a6460509d407
dburnet3 <deniseburnett26@gmail.com>
2019-09-04 15:37:52 -0400

    this should fix it...

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -11,16 +11,7 @@ module.exports = {
     },
     findOneAndUpdate: function (req, res) {
         db.Activity
-        // .update({ _id: req.params.id }, { where: req.body })
-        // .update(req.body, {
-        //     where: {
-        //         title: req.body.title,
-        //         time_slots: req.body.time_slots,
-        //         dow: req.body.dow,
-        //         taken: req.body.taken
-        //     }
-        // })
-        findOneAndUpdate({ _id: req.params.id }, req.body)
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
             .then(dbActivity => res.json(dbActivity))
             .catch(err => res.status(422).json(err));
     },
