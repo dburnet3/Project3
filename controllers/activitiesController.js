3ab8ec183dce4fa9cb7bb6857e8fb1e437db71ba
dburnet3 <deniseburnett26@gmail.com>
2019-09-04 16:07:51 -0400

    hoping this works

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -9,13 +9,13 @@ module.exports = {
             .then(dbActivity => res.json(dbActivity))
             .catch(err => res.status(422).json(err));
     },
-    find: function (req, res) {
+    findOne: function (req, res) {
         console.log("hello---------------work-------------please------------");
         console.log(req);
         console.log(req.params.id);
         console.log(req.body);
         db.Activity
-            .find({ id: req.params.id }, req.body)
+            .findOne({ id: req.params.id }, req.body)
             .then(dbActivity => res.json(dbActivity))
             .catch(err => res.status(422).json(err));
     },
