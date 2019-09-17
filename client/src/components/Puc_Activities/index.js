b3455436f5018dc6f14cc0e9accabf66fc584ab7
dburnet3 <deniseburnett26@gmail.com>
2019-09-17 15:59:27 -0400

    added a route

--- client/src/components/Puc_Activities/index.js
+++ client/src/components/Puc_Activities/index.js
@@ -12,6 +12,8 @@ import '../Puc_Creation';
 import notification from '../../assets/images/notification.png';
 import './puc_activities.css';
 // import Select from 'react-select';
+import { BrowserRouter as Route, Link } from 'react-router-dom';
+import Activities from '../Activities/index';
 
 //This is the selectable dropdown that can be used to select the day of the week
 
@@ -148,7 +150,7 @@ class Puc_Activities extends Component {
                             </Label>
                         </FormGroup>
                         <FormGroup>
-                            <Button onClick={this.handleFormSubmitPucActivities}>Add</Button>
+                            <Link to={'../Activities'}><Route path='../Activities' component={Activities}> <Button onClick={this.handleFormSubmitPucActivities}>Add</Button></Route></Link>
                         </FormGroup>
                     </Col>
                     <Row>
